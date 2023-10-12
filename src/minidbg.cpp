d1aac543ed1d4e7f0f4df90eafc82319022ea5e6
rrjyy <2736300679@qq.com>
2023-10-12 18:57:03 +0800

    source and signals

--- src/minidbg.cpp
+++ src/minidbg.cpp
@@ -6,6 +6,11 @@
 #include <sstream>
 #include <iomanip>
 #include <sys/personality.h>
+#include <fstream>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+#include <sys/types.h>
 
 #include "linenoise.h"
 
@@ -34,6 +39,119 @@ bool is_prefix(const std::string& s, const std::string& of)
     return std::equal(s.begin(), s.end(), of.begin());
 }
 
+dwarf::die debugger::get_function_from_pc(uint64_t pc)
+{
+    for (auto &cu : m_dwarf.compilation_units())
+    {
+        if (die_pc_range(cu.root()).contains(pc))
+        {
+            for (const auto& die : cu.root())
+            {
+                if (die.tag == dwarf::DW_TAG::subprogram)
+                {
+                    if (die_pc_range(die).contains(pc))
+                    {
+                        return die;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("1\n");
+    throw std::out_of_range{"Cannot find function"};
+}
+
+dwarf::line_table::iterator debugger::get_line_entry_from_pc(uint64_t pc)
+{
+    for(auto &cu : m_dwarf.compilation_units())
+    {
+        if(die_pc_range(cu.root()).contains(pc))
+        {
+            auto &lt = cu.get_line_table();
+            auto it = lt.find_address(pc);
+            if(it == lt.end())
+            {
+                printf("2\n");
+                throw std::out_of_range{"Cannot find line entry"};
+            }
+            else
+            {
+                return it;
+            }
+        }
+    }
+
+    printf("3\n");
+    throw std::out_of_range{"Cannot find line entry"};
+}
+
+void debugger::initialise_load_address()
+{
+    //If this is a dynamic library (e.g. PIE)
+    if (m_elf.get_hdr().type == elf::et::dyn)
+    {
+        //The load address is found in /proc/&lt;pid&gt;/maps
+        std::ifstream map("/proc/" + std::to_string(m_pid) + "/maps");
+
+        //Read the first address from the file
+        std::string addr;
+        std::getline(map, addr, '-');
+
+        m_load_address = std::stoull(addr, 0, 16); // stoi may out of range of int
+
+    }
+}
+
+uint64_t debugger::offset_load_address(uint64_t addr)
+{
+    return addr - m_load_address;
+}
+
+void debugger::print_source(const std::string& file_name, unsigned line, unsigned n_lines_context)
+{
+    std::ifstream file {file_name};
+
+    //Work out a window around the desired line
+    auto start_line = line <= n_lines_context ? 1 : line - n_lines_context;
+    auto end_line = line + n_lines_context + (line < n_lines_context ? n_lines_context - line : 0) + 1;
+
+    char c{};
+    auto current_line = 1u;
+    //Skip lines up until start_line
+    while(current_line != start_line && file.get(c))
+    {
+        if (c == '\n')
+        {
+            ++current_line;
+        }
+    }
+
+    //Output cursor if we're at the current line
+    std::cout << (current_line == line ? "> " : " ");
+
+    //Write lines up until end_line
+    while (current_line <= end_line && file.get(c))
+    {
+        std::cout << c;
+        if (c == '\n')
+        {
+            ++current_line;
+            std::cout << (current_line == line ? "> " : " ");
+        }
+    }
+
+    //Write newline and make sure that the stream is flushed properly
+    std::cout << std::endl;
+}
+
+siginfo_t debugger::get_signal_info()
+{
+    siginfo_t info;
+    ptrace(PTRACE_GETSIGINFO, m_pid, nullptr, &info);
+    return info;
+}
+
 uint64_t debugger::get_pc()
 {
     return get_register_value(m_pid, reg::rip);
@@ -46,17 +164,11 @@ void debugger::set_pc(uint64_t pc)
 
 void debugger::step_over_breakpoint()
 {
-    // -1 because execution will go past the breakpoint
-    auto possible_breakpoint_location = get_pc() - 1;
-
-    if (m_breakpoints.count(possible_breakpoint_location))
+    if (m_breakpoints.count(get_pc()))
     {
-        auto& bp = m_breakpoints[possible_breakpoint_location];
+        auto& bp = m_breakpoints[get_pc()];
         if (bp.is_enabled())
         {
-            auto previous_instruction_address = possible_breakpoint_location;
-            set_pc(previous_instruction_address);
-
             bp.disable();
             ptrace(PTRACE_SINGLESTEP, m_pid, nullptr, nullptr);
             wait_for_signal();
@@ -70,6 +182,44 @@ void debugger::wait_for_signal()
     int wait_status;
     auto options = 0;
     waitpid(m_pid, &wait_status, options);
+
+    auto siginfo = get_signal_info();
+
+    switch(siginfo.si_signo)
+    {
+        case SIGTRAP:
+            handle_sigtrap(siginfo);
+            break;
+        case SIGSEGV:
+            std::cout << "Yay, segfault. Reason: " << siginfo.si_code << std::endl;
+            break;
+        default:
+            std::cout << "Got signal " << strsignal(siginfo.si_signo) << std::endl;
+    }
+}
+
+void debugger::handle_sigtrap(siginfo_t info)
+{
+    switch (info.si_code)
+    {
+        //one of these will be set if a breakpoint was hit
+        case SI_KERNEL:
+        case TRAP_BRKPT:
+        {
+            set_pc(get_pc() - 1); //put the pc back where it should be
+            std::cout << "Hit breakpoint at address 0x" << std::hex << get_pc() << std::endl;
+            auto offset_pc = offset_load_address(get_pc()); //for querying DWARF
+            auto line_entry = get_line_entry_from_pc(offset_pc);
+            print_source(line_entry->file->path, line_entry->line);
+            return;
+        }
+        //this will be set if the signal was sent by single stepping
+        case TRAP_TRACE:
+            return;
+        default:
+            std::cout << "Unknown SIGTRAP code " << info.si_code << std::endl;
+            return;
+    }
 }
 
 uint64_t debugger::read_memory(uint64_t address)
@@ -150,9 +300,9 @@ void debugger::handle_command(const std::string& line)
 
 void debugger::run()
 {
-    int wait_status;
-    auto options = 0;
-    waitpid(m_pid, &wait_status, options);
+    wait_for_signal();
+    printf("after wait\n");
+    initialise_load_address();
 
     char* line = nullptr;
     while((line = linenoise("minidbg > ")) != nullptr)
---
+++ test/debug_test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	long a = 3;
+	long b = 2;
+	long c = a + b;
+	a = 4;
+	return 0;
+}
